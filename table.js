92963afa81f3f59f47cf811828d830629d97ab59
skc4365 <50658047+skc4365@users.noreply.github.com>
2026-07-15 11:18:27 +0900

    edit 프로젝트-repo2

--- table.js
+++ table.js
@@ -135,7 +135,7 @@ function createTable(tableData) {
         </td>
         <td style="padding: 10px; text-align: center;">
           <strong>${userName}</strong><br>
-          <a href="https://github.com/${githubId}/${item.link99}" target="_blank" 
+          <a href="https://github.com/${item.link99}" target="_blank" 
              style="color: #0066cc; text-decoration: none; font-size: 0.9em;">
             ${item.link99}
           </a>
